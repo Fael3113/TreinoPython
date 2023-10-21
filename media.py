bc4698a2209b2e5cdd623534b34314b818633fff
Fael3113 <santiagorafaelsantiago@hotmail.com>
2023-10-21 11:06:59 -0300

    Update media.py

--- media.py
+++ media.py
@@ -1,11 +1,16 @@
 nome = input('Digite seu nome: ')
-num1 = float(input('Digite sua primeira nota: '))
-num2 = float(input('Digite sua segunda nota: '))
-num3 = float(input('Digite sua terceira nota: '))
-num4 = float(input('Digite sua quarta nota: '))
-resultado = (num1 + num2 + num3 + num4) / 4
-if resultado>=7:
+media = 0
+for contador in range (1,5,1):
+    nota = float(input(f'Insira a {contador}ª notado do aluno: '))
+    media = media + nota 
+    resultado = media/4
+faltas = int(input('Digite o numero de faltas: '))
+if resultado>=7 and faltas<=5:
     print('Aprovado')
+elif resultado>=7 and faltas>5:
+    print('Reprovado por falta')
+elif resultado<7 and faltas<=5:
+    print('Reprovado por media')        
 else:
     print('Reprovado')
 print('Nome do Aluno:', nome)
